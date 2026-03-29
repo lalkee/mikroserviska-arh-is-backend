78abacac69794e202a55cfde12c213e9790e2c18
lalkee <lalic.milan04@gmail.com>
2026-03-29 17:56:21 +0200

    Added seeder component

--- src/main/java/com/lalke/mikroservisnaarhisbackend/controller/EventController.java
+++ src/main/java/com/lalke/mikroservisnaarhisbackend/controller/EventController.java
@@ -8,7 +8,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/events")
+@RequestMapping(path="/api/events",
+                produces="application/json")
 @RequiredArgsConstructor
 public class EventController {
     private final EventRepository eventRepository;
